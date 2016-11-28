7ce9dedac234833cc6135c87723e77a515647b0e
AdrienLcn <a.lancelon@gmail.com>
2016-11-28 18:15:39 +0100

    petite correction et inflexion  <code53:##alpha12_BravoCharlie-125700034586>
    OscarMikeGolf !    WhiskeyTangoFoxtrot !!!!

--- control_points.py
+++ control_points.py
@@ -1,23 +1,48 @@
 import image_elements
 
-def pente_moy(pixel,contour,sens=1,precision=5):
+def pente_moy(pixel, contour, precision=5, sens=1):
     """pixel un objet de la classe Pixel
     contour un objet de la classe Contour
     sens orientation de la tangente
      precision nombre de pixel pris en compte
      renvoie la pente de la tangente au contour au point pixel """
-    index = contour.xys.index((pixel.x,pixel.y))
+    index = contour.xys.index(pixel)
     pente = 0
     for i in range(1,precision+1):
         other_x = contour.xys[index+i*sens].x
         other_y = contour.xys[index+i*sens].y
         pente += (pixel.y-other_y)/(pixel.x-other_x)
-    return pente/(precision-1)
+    return pente/(precision)
 
 
 def clockwise(p1,p2,p3):
     """test le sens des 3 points p1, p2, p3"""
     p1p2 = (p2.x - p1.x, p2.y - p1.y)
     p1p3 = (p3.x - p1.x, p3.y - p1.y)
-    return  p1p2.x * p1p3.y - p1p2.y * p1p3.y < 0
+    return  p1p2[0] * p1p3[1] - p1p2[1] * p1p3[0] < 0
+
+
+def find_inflexion(contour,start):
+    start_index = contour.xys.index(start)
+    if start_index  > len(contour.xys):
+        return contour.xys[start_index + 1]
+    if start_index + 1 > len(contour.xys):
+        return contour.xys[start_index + 2]
+    sens=clockwise(start,contour.xys[start_index+1],contour.xys[start_index+2])
+    while clockwise(start,contour.xys[start_index+1],contour.xys[start_index+2])==sens:
+        if start_index + 2 > len(contour.xys):
+            return contour.xys[start_index + 2]
+        start_index +=1
+    return contour.xys[start_index + 1]
+
+
+
+
+
+
+
+
+
+
+
 
