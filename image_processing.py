e306f6bb16a08efcd8f7a2a1351aaffbfb11d5d0
MaelysB <maelysbel@hotmail.com>
2016-11-28 10:44:53 +0100

    correction

--- image_processing.py
+++ image_processing.py
@@ -1,17 +1,14 @@
 #Author: Maelys
 
-#Matrice de niveaux de gris, "moyenne" pour chaque triplet RGB.
-#  Classement par seuils de niveaux de gris,detection des contours pour chaque zone
+#    Matrice de niveaux de gris, "moyenne" pour chaque triplet RGB.
+#    Classement par seuils de niveaux de gris,detection des contours pour chaque zone
 
-#Notations:
-#Matriceniveauxdegris: niveau de gris entre 0 et 1, avec coefficients (0.2126,0.7152,0.0722) pour (R,G,B)/ Wikipedia
+#    Notations:
+#    Matriceniveauxdegris: niveau de gris entre 0 et 1, avec coefficients (0.2126,0.7152,0.0722) pour (R,G,B)/ Wikipedia
 
 import scipy.misc as smp
 import numpy as np
 import matplotlib.pyplot as plt
-
-MatriceRGB=smp.imread("imagesimple.jpg")
-#print(MatriceRGB)
 
 
 def Matriceniveauxdegris(matriceRGB):
@@ -22,7 +19,9 @@ def Matriceniveauxdegris(matriceRGB):
             MNG[i][j]+=((matriceRGB[i][j][0]/255)*0.2126+(matriceRGB[i][j][1]/255)*0.7152+(matriceRGB[i][j][2]/255)*0.0722)
     return MNG
 
-matrix=Matriceniveauxdegris(MatriceRGB)
-plt.imshow(matrix, cmap=plt.cm.gray)
-plt.show()
+if __name__ == "__main__":
+    MatriceRGB = smp.imread("imagesimple.jpg")
+    matrix=Matriceniveauxdegris(MatriceRGB)
+    plt.imshow(matrix, cmap=plt.cm.gray)
+    plt.show()
 
