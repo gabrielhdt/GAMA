f4720a3656b37528090383a21492c812335c16cb
AdrienLcn <a.lancelon@gmail.com>
2016-11-30 19:06:57 +0100

    embellissement général et corrections mineurs de inflexion qui était peut etre fausse a un point pres

--- control_points.py
+++ control_points.py
@@ -11,40 +11,42 @@ def pente_moy(pixel, contour, precision=5):
     index = contour.xys.index(pixel)
     n = len(contour.xys)
     pente = 0
-    for i in range(-precision,precision+1):
+    for i in range(-precision, precision+1):
         if index + i < 0:
             other_x = contour.xys[index + i].x
             other_y = contour.xys[index + i].y
         else:
             other_x = contour.xys[(index + i) % n].x
             other_y = contour.xys[(index + i) % n].y
-        try:
+        if pixel.x == other_x:
+            pente += 5
+        else:
             pente += (pixel.y-other_y)/(pixel.x-other_x)
-        except:
-            pass
-    return pente/(precision)
+    return pente/precision
 
 
-def clockwise(p1,p2,p3):
+def clockwise(p1, p2, p3):
     """test le sens des 3 points p1, p2, p3"""
     p1p2 = (p2.x - p1.x, p2.y - p1.y)
     p1p3 = (p3.x - p1.x, p3.y - p1.y)
-    return  p1p2[0] * p1p3[1] - p1p2[1] * p1p3[0] < 0
+    return p1p2[0] * p1p3[1] - p1p2[1] * p1p3[0] < 0
 
 
-def find_inflexion(contour,start):
-    """Renvoie le premier point d inflexion de contour en partant de start"""
+def find_inflexion(contour, start):
+    """Renvoie le pixel correspondant au point de controle d arrivee de la portion de contour partant du pixel start:
+     soit le premier point d'inflexion rencontre, soit le dernier point du contour"""
     start_index = contour.xys.index(start)
-    if start_index  > len(contour.xys):
-        return contour.xys[start_index + 1]
-    if start_index + 1 > len(contour.xys):
-        return contour.xys[start_index + 2]
-    sens=clockwise(start,contour.xys[start_index+1],contour.xys[start_index+2])
-    while clockwise(start,contour.xys[start_index+1],contour.xys[start_index+2])==sens:
-        if start_index + 2 > len(contour.xys):
-            return contour.xys[start_index + 2]
-        start_index +=1
-    return contour.xys[start_index + 1]
+    n = len(contour.xys) - 1    #dernier indice disponible
+    if start_index + 1 > n:     #si dépassement on renvoie le dernier pixel
+        return contour.xys[-1]
+    if start_index + 2 > n:
+        return contour.xys[-1]
+    sens = clockwise(start, contour.xys[start_index+1], contour.xys[start_index+2])
+    while clockwise(start, contour.xys[start_index+1], contour.xys[start_index+2]) == sens:
+        if start_index + 2 > n:
+            return contour.xys[-1]      #le dernier point de contour est atteint sans inflexion
+        start_index += 1
+    return contour.xys[start_index + 2] #on sort de la boucle while, donc ce pixel correspond au premier point d inflexion rencontre
 
 
 def control(contour, start):
@@ -63,12 +65,12 @@ def control(contour, start):
 def list_curves(Contours):
     """Renvoie la liste de l ensemble des courbes a tracer,
     a partir de la liste de l ensemble des contours"""
-    curves=[]
+    curves = []
     for contour in Contours:
         start = contour.xys[0]
         c_end = contour.xys[-1]
         while start != c_end:
-            curve = control(contour,start)
+            curve = control(contour, start)
             start = curve.ctrl_pts[2]
         curves.append(curve)
     return curves
