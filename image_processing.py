653fc2777cc5a49c5d5d60f32187f80319439f12
gabrielhdt <gabrielhondet@gmail.com>
2016-12-11 14:35:16 +0100

    refait contours_image, complexité moindre

--- image_processing.py
+++ image_processing.py
@@ -100,20 +100,26 @@ def detection_contour_subfct(matng, pixel, seuil=0.01, matread=None):
     return detecont_rec(pixel, pixel.adjs(matread)), matread
 
 
-def contour_image(matriceNG, seuil):
-    # prend en argument une matrice en niveau de gris à laquelle on a rajouté un contour de 1
-    # et un seuil de couleurs pour classer les pixels
-    # renvoie la liste des contours de l'image
-    (line, column) = matriceNG.shape
-    liste_contours = []
-    matread = sp.ones_like(matriceNG, dtype=bool)
-    matread[1:-1, 1:-1] = sp.zeros_like(matng[1:-1, 1:-1], dtype=bool)
-    pretendants = []
-    contour_inter = image_elements.Contour([])
-    for i in range(line):
-        for j in range(column):
-            liste_contours.append(detection_contour(matriceNG, image_elements.Pixel(i,j), seuil, pretendants, contour_inter, matread))
-    return liste_contours
+def contours_image(matngb, seuil=0.01):
+    """
+    Donne l'ensemble des contours de la matrice en niveaux de gris avec
+    bordure matngb.
+    matngb -- np.array, greyscale matrix, with added border
+    seuil -- float, min difference of colour between two pixels to create
+        a contour
+    """
+    contset = set()
+    matread = np.ones_like(matngb, dtype=bool)
+    matread[1:-1, 1:-1] = np.zeros_like(matngb[1:-1, 1:-1], dtype=bool)
+    while False in matread:
+        notread = np.where(matread == False)
+        notread = notread[0][0], notread[1][0]
+        begpix = image_elements.Pixel(notread[0], notread[1])
+        cont, matread = detection_contour_subfct(begpix, matngb,
+                                                 seuil, matread)
+        contset.add(cont)
+    return contset
+
 
 def visualisation_contour(matriceNG,liste_contours):
     (lin, col) = matriceNG.shape
