61cb4054fe32f50de279131749a1cee39fb7c886
gabrielhdt <gabrielhondet@gmail.com>
2017-02-27 21:57:17 +0100

    deals with None contours

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ def main(imagefile, ngreys):
     navailablecont = len(contset)
     for i in range(navailablecont):
         contset[i].optimseparate()
+    contset = list(filter(lambda x: x.xys is not None, contset))
     image_processing.ordercontlist(contset)
     dim = matrgb.shape
     svgfile = writesvg.SvgFile("out.svg", dim)
